462e32eadedb393a865f2acc8885206ff484fc98
Bensnuf <68095081+Bensnuf@users.noreply.github.com>
2020-08-20 15:56:43 -0500

    typed more to help understand the code better

--- website/tests.js
+++ website/tests.js
@@ -1,22 +1,27 @@
 /* Global Variables */
-flet baseURL = "http://api.openweathermap.org/data/2.5/weather?zip=";
+let baseURL = "http://api.openweathermap.org/data/2.5/weather?zip=";
 let apiKey = "&appid=3f600919bbab6d72305cefa26478e9a7&units=imperial";
+
 
 // Async function and fetch() for Get request
 document.getElementById("generate").addEventListener("click", performAction);
 
+
 function performAction(e) {
-    const zipCode = document.getElementById("zip").value;  //what is .value?
+const zipCode = document.getElementById("zip").value; //.value grabs what's typed inside the "zip" and not
+                                                      // the box itself, it must be inside the function so
+                                                      // it always grabs the most recently typed zip
     getCity(baseURL, zipCode, apiKey);
 }
 
-const getCity = async (baseURL, zip, key) => {        //don't fully understand this function and why it works
-    const res = await fetch(baseURL + zip + key);
+const getCity = async (x, y, z) => {  // can be changed from (baseURL, zip, key) and work fine,
+                                      // what's actually typed here 100% doesn't matter cause the function
+                                      // performAction() places what we actually want when we click
+  const res = await fetch(x + y + z);
     try {
-      
         const data = await res.json();
         console.log(data);
-        return data;
+        // return data;               // may not need, still requires testing
     } catch (error) {
         console.log("error", error);
     }
